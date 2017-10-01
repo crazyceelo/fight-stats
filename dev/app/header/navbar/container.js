b7c995cb2bc2f0729d1d00abfe5a5e5b24f1b2f7
bryancn2009 <bryancn_2009@yahoo.com>
2017-10-01 15:42:33 -0700

    fixed live server connection

--- dev/app/header/navbar/container.js
+++ dev/app/header/navbar/container.js
@@ -7,6 +7,9 @@ import {connect} from 'react-redux';
 //Import local dependencies
 import NavbarComp from './component';
 
+//Connect to live server
+const socket = (window.location.hostname == 'localhost' ? io.connect("localhost:3000") : io.connect(window.location.hostname));
+
 class Navbar extends Component{
     componentDidMount(){
         $(".button-collapse").sideNav();        
