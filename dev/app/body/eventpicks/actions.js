3565d9bbb5811ead9d3fd34ce73fe36414a0a77f
Carlos Perez <solracias@gmail.com>
2017-10-08 11:11:46 -0700

    created an object but still not getting to reducer

--- dev/app/body/eventpicks/actions.js
+++ dev/app/body/eventpicks/actions.js
@@ -29,6 +29,8 @@ export const handleSubmit = (event) => {
     var eventObject = event.target.querySelectorAll('[data-eventpicks-fighter]');
     var picks = [];
     var pick = {};
+    var allPicks = {};
+    // grab each attribute for each li and put into array
     for (var i = 0; i < eventObject.length; i++){
         var fighter = eventObject[i].getAttribute('data-eventpicks-fighter');
         picks.push(fighter);
@@ -37,25 +39,26 @@ export const handleSubmit = (event) => {
         var eventName = eventObject[i].getAttribute('data-eventpicks-event_name');
         picks.push(eventName);
         
+        // split array into groups of 3
         var a, j, temparray, chunk = 3;
         for (a=0, j=picks.length; a < j; a+=chunk){
             temparray = picks.slice(a, a+chunk);
         }
-        // console.log(temparray);
+        // put array in an object
         for (var x = 0; x < temparray.length; x++){
             pick[x] = temparray[x];
         }
+        // this console logs the propery array of objects.
         console.log(pick);
     }
 
-    /* // need to replace with array instead of index 0
+    
     return({
         type: 'SUBMIT_PICKS',
         payload: {
-            fighter: eventObject[0].getAttribute('data-eventpicks-fighter'),
-            event_name: eventObject[0].getAttribute('data-eventpicks-event_id'),
-            event_id: eventObject[0].getAttribute('data-eventpicks-event_name')
+            // !! this is only logging the first object !!
+            pick: pick
         }
     })
-    */
+    
 };
