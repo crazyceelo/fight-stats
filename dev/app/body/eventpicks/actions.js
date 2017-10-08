a776e4f3dee808bc77bfc424a27990640ae98ced
Carlos Perez <solracias@gmail.com>
2017-10-08 10:49:54 -0700

    turned array into an object now

--- dev/app/body/eventpicks/actions.js
+++ dev/app/body/eventpicks/actions.js
@@ -27,6 +27,26 @@ export const handleSubmit = (event) => {
     */
 
     var eventObject = event.target.querySelectorAll('[data-eventpicks-fighter]');
+    var picks = [];
+    var pick = {};
+    for (var i = 0; i < eventObject.length; i++){
+        var fighter = eventObject[i].getAttribute('data-eventpicks-fighter');
+        picks.push(fighter);
+        var eventId = eventObject[i].getAttribute('data-eventpicks-event_id');
+        picks.push(eventId);
+        var eventName = eventObject[i].getAttribute('data-eventpicks-event_name');
+        picks.push(eventName);
+        
+        var a, j, temparray, chunk = 3;
+        for (a=0, j=picks.length; a < j; a+=chunk){
+            temparray = picks.slice(a, a+chunk);
+        }
+        // console.log(temparray);
+        for (var x = 0; x < temparray.length; x++){
+            pick[x] = temparray[x];
+        }
+        console.log(pick);
+    }
 
     /* // need to replace with array instead of index 0
     return({
